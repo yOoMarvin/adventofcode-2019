3a70991ded05aeae5c8f8ffa599a820ded49c145
yoomarvin <marvin.messenzehl@gmail.com>
2019-12-03 14:45:57 +0100

    day 3 part 1 and I hate everything visual

---
+++ 03-crossed-wires/puzzle-01.js
@@ -0,0 +1,44 @@
+var fs = require("fs");
+var input = fs.readFileSync("./input.txt", "utf8");
+
+const getWirePositions = wire => {
+  const positions = new Set();
+  const position = { x: 0, y: 0 };
+
+  for (let i = 0; i < wire.length; i++) {
+    const { direction, distance } = wire[i];
+
+    for (let step = 1; step <= distance; step++) {
+      position.x += direction === "L" ? -1 : direction === "R" ? 1 : 0;
+      position.y += direction === "U" ? -1 : direction === "D" ? 1 : 0;
+
+      positions.add(`${position.x},${position.y}`);
+    }
+  }
+
+  return positions;
+};
+
+// prep the input
+const wires = input.split("\n").map(wire => {
+  return wire
+    .trim()
+    .split(",")
+    .map(instruction => {
+      return {
+        direction: instruction[0],
+        distance: Number(instruction.slice(1))
+      };
+    });
+});
+
+const wire1Positions = getWirePositions(wires[0]);
+const wire2Positions = [...getWirePositions(wires[1])];
+
+console.log(
+  wire2Positions
+    .filter(position => wire1Positions.has(position))
+    .map(position => position.split(",").map(Number))
+    .map(([x, y]) => Math.abs(x) + Math.abs(y))
+    .sort((a, b) => a - b)[0]
+);
