3647e4752153c2a88b798c7e5c487fc3bc91f67a
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2019-12-03 21:42:53 +0100

    damn I'm bad at visuals 🤷‍♂️

--- 03-crossed-wires/puzzle-02.js
+++ 03-crossed-wires/puzzle-02.js
@@ -1,12 +1,16 @@
 var fs = require("fs");
 var input = fs.readFileSync("./input.txt", "utf8");
 
+// function to get the wire positions
 const getWirePositions = wire => {
-  const positions = new Set();
+  const positions = new Map();
   const position = { x: 0, y: 0 };
+
+  let length = 0;
 
   // loop over the complete wire
   for (let i = 0; i < wire.length; i++) {
+    // save into the object
     const { direction, distance } = wire[i];
 
     for (let step = 1; step <= distance; step++) {
@@ -14,14 +18,15 @@ const getWirePositions = wire => {
       position.x += direction === "L" ? -1 : direction === "R" ? 1 : 0;
       position.y += direction === "U" ? -1 : direction === "D" ? 1 : 0;
 
-      positions.add(`${position.x},${position.y}`);
+      // add the positions and length to the map
+      positions.set(`${position.x},${position.y}`, ++length);
     }
   }
 
   return positions;
 };
 
-// prep the input
+// pre process input
 // split into the two wires
 // split by comma to get the instructions
 // return object with direction and distance (as int)
@@ -39,17 +44,12 @@ const wires = input.split("\n").map(wire => {
 
 const wire1Positions = getWirePositions(wires[0]);
 // spread to array, so we can filter on that later
-const wire2Positions = [...getWirePositions(wires[1])];
+const wire2Positions = [...getWirePositions(wires[1]).entries()];
 
 // main execution
-// filter only the wire2 elements that match the position at wire1
-// map over those positions and split them on the ","
-// map over those and add them
-// sort and take the smallest
 console.log(
   wire2Positions
-    .filter(position => wire1Positions.has(position))
-    .map(position => position.split(",").map(Number))
-    .map(([x, y]) => Math.abs(x) + Math.abs(y))
+    .filter(([position]) => wire1Positions.has(position))
+    .map(([position, steps]) => wire1Positions.get(position) + steps)
     .sort((a, b) => a - b)[0]
 );
