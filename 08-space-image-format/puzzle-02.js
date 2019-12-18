ec757ec1345851b05c92b837a8cb7dad86e66e53
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2019-12-18 08:31:50 +0100

    took me endless time to find the right symbol for the print

--- 08-space-image-format/puzzle-02.js
+++ 08-space-image-format/puzzle-02.js
@@ -1,51 +1,46 @@
 const fs = require("fs");
-const input = fs.readFileSync("./input.txt", "utf8").split("");
-//const input = "0222112222120000".split("");
+const read = fs.readFileSync("./input.txt");
+let data = read
+  .toString()
+  .split("")
+  .map(Number);
 
-const wide = 25;
-const tall = 6;
+const width = 25;
+const height = 6;
 
-const layers = new Object();
+let layers = [];
+let w = 0;
+let h = 0;
+let layer = [];
 
-let index = 0;
-
-// prep the input object like in the puzzle description
-for (let t = 1; t <= tall; t++) {
-  layers[t] = [];
-  for (let x = 0; x < tall; x++) {
-    layers[t][x] = [];
-    for (let w = 1; w <= wide; w++) {
-      layers[t][x].push(input[index]);
-      index++;
-    }
-  }
+for (let i = 0; i < data.length; i++) {
+  layer.push(data[i]);
+  w++;
+  w == width
+    ? (h++,
+      (w = 0),
+      h == height ? (layers.push(layer), (h = 0), (layer = [])) : undefined)
+    : undefined;
 }
 
-// prep image with all black pixels
 let image = [];
-for (let t = 0; t < tall; t++) {
-  image[t] = [];
-  for (let w = 0; w < wide; w++) {
-    image[t][w] = "-";
+for (let r = 0; r < width * height; r++) {
+  let j = 0;
+  let color = layers[j][r];
+  while (color == 2) {
+    color = layers[j][r];
+    j++;
   }
+  image.push(color);
 }
-
-// loop through the layers
-Object.entries(layers).forEach(([index, layer]) => {
-  console.log(index);
-  for (let i = 0; i < layer.length; i++) {
-    for (let j = 0; j < layer[i].length; j++) {
-      // problem... if a layer later on has the value it overwrites the previous one
-      layers[index][i][j] === "0" ? (image[i][j] = "0") : null;
-    }
-  }
-});
-
-// print image
+let k = 0;
+let img = "";
 for (let i = 0; i < image.length; i++) {
-  let str = "";
-  for (let j = 0; j < image[i].length; j++) {
-    str = str.concat(image[i][j]);
+  k++;
+  image[i] == 1 ? (img = img + "▮") : (img = img + " ");
+  if (k == 25) {
+    k = 0;
+    console.log(img);
+    img = "";
   }
-  console.log(str);
 }
